f02f4d8c5c170d4748b3aee3a34e48a54597a390
Daniil <tornado9815@mail.ru>
2024-08-13 18:27:50 +0300

    Fix

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int phoneBalance = 1000;
-        int replenishment = 446;
-        int replenishment2 = 2235;
-        if(replenishment < 1000){
+        int replenishment = 1200;
+        int bonus = replenishment / 100 * 1;
+        if (replenishment < 1000) {
             System.out.println("Счет пополнен на " + replenishment);
             System.out.println("Бонусных рублей начислено: 0");
             System.out.println("Всего на счету: " + (replenishment + phoneBalance));
-        }
-        System.out.println("_____________________________");
-        if (replenishment2 > 1000){
-            System.out.println("Счет пополнен на " + replenishment2);
-            int bonus = replenishment2 / 100 * 1;
+        } else {
+            System.out.println("Счет пополнен на " + replenishment);
             System.out.println("Бонусных рублей начислено: " + bonus);
-            System.out.println("Всего на счету: " + (phoneBalance + replenishment2 + bonus));
+            System.out.println("Всего на счету: " + (phoneBalance + replenishment + bonus));
         }
     }
 }
